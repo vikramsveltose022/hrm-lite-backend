f14ce5339b1f2baf8c4653a8e5d7d24750fbf556
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2024-07-18 17:45:01 +0530

    master-login

---
+++ model/customer.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const CustomerSchema = new mongoose.Schema({
+    ShopImage: {
+        type: String
+    },
+    Name: {
+        type: String
+    },
+    ShopName: {
+        type: String
+    },
+    Address: {
+        type: String
+    },
+    State: {
+        type: String
+    },
+    City: {
+        type: String
+    },
+    Email: {
+        type: String
+    },
+    Password: {
+        type: String
+    },
+    Contact: {
+        type: String
+    },
+    GstNo: {
+        type: String
+    },
+    Pincode: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: "Active"
+    }
+
+}, { timestamps: true });
+
+export const Customer = mongoose.model("customer", CustomerSchema);
---
+++ routes/customer.route.js
@@ -0,0 +1,26 @@
+import express from "express"
+import multer from "multer";
+import path from "path"
+import { deleteCustomerDetail, saveCustomerDetails, updatedCustomerDetail, viewCustomerDetail, viewCustomerDetailById } from "../controller/customer.controller.js";
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/Images/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const fileExtension = path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
+    }
+});
+const upload = multer({ storage: storage });
+
+router.post("/save-customer", upload.single("image"), saveCustomerDetails);
+router.get("/view-customer", viewCustomerDetail)
+router.get("/view-customer-by-id/:id", viewCustomerDetailById)
+router.delete("/delete-customer/:id", deleteCustomerDetail)
+router.put("/update-customer/:id", upload.single("image"), updatedCustomerDetail)
+
+export default router;
